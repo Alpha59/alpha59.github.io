b24e2fdf4f5559c68fefab042a6624e067fcf403
Jake Ailor <ailor.john.r@gmail.com>
2018-05-01 17:01:09 -0400

    Initial commit

---
+++ make.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+rm -rf output/
+rm -rf finals/
+mkdir finals/
+cp -r ./templates/* finals/
+
+hackmyresume validate resume.json
+hackmyresume analyze resume.json
+nunjucks templates/index.html resume.json -o output/ -u --options nunjucks-html.json
+
+mkdir ./output/awesome-cv/
+cp -r ./awesome-cv/* ./output/awesome-cv/
+
+nunjucks awesome-cv/resume.tex resume.json -o output/ -u --options nunjucks.json
+
+for file in ./awesome-cv/resume/*
+do
+    nunjucks $file resume.json -o output/ -u --options nunjucks.json
+done
+
+mv ./output/awesome-cv/resume.html ./output/awesome-cv/resume.tex
+
+for file in ./output/awesome-cv/resume/*.html
+do
+    mv "$file" ./output/awesome-cv/resume/"$(basename "$file" .html).tex"
+done
+
+cd ./output/awesome-cv
+pwd
+xelatex resume.tex
+
+#pdflatex personal-cv/main.tex
+#pdflatex awesome-cover/main.tex
+#lualatex -interaction nonstopmode old-cv/resume.tex
+#pdflatex superbland-cv/resume.tex
+
+# update LinkedIn: https://www.npmjs.com/package/node-linkedin
+
+cd ../../
+cp ./output/awesome-cv/resume.pdf ./finals/John_Ailor.pdf
+cp ./output/templates/index.html ./finals/index.html
+
+#rm -rf output/
