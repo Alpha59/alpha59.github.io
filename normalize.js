5023ca8b50e4cb1ead3c0f3c9300952941483a60
Jake Ailor <ailor@amazon.com>
2021-05-13 00:43:42 -0700

    Update build logic

--- normalize.js
+++ normalize.js
@@ -1,10 +1,9 @@
 "use strict";
-const flatten = require('flat')
 const fs = require("fs");
-const resume = JSON.parse(fs.readFileSync("resume.json"));
+const resume = JSON.parse(fs.readFileSync("../resume.json"));
 const resume_strict = Object.assign({}, resume);
 
 ['work', 'education', 'volunteer', 'project'].map((ty) =>
-        resume[ty].map((el, i) => el.highlights ? resume_strict[ty][i].highlights = Object.values(flatten(el.highlights)) : undefined));
+    (resume[ty] || []).map((el, i) => el.highlights ? resume_strict[ty][i].highlights = Object.values(el.highlights).map(el => typeof el === "object" ? Object.values(el).flat(): el).flat() : undefined));
 
 fs.writeFileSync('John_Ailor.json', JSON.stringify(resume_strict, null, 2));
