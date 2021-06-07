1c6b13bdd93570071de6809246b54da2b7962bab
Jake Ailor <ailor@amazon.com>
2021-06-06 19:15:28 -0700

    Update for base AMZN Experiance

--- normalize.js
+++ normalize.js
@@ -1,9 +1,10 @@
 "use strict";
 const fs = require("fs");
-const resume = JSON.parse(fs.readFileSync("../resume.json"));
+const args = process.argv.slice(2);
+const resume = JSON.parse(fs.readFileSync(`../${args[0]}`));
 const resume_strict = Object.assign({}, resume);
 
 ['work', 'education', 'volunteer', 'project'].map((ty) =>
-    (resume[ty] || []).map((el, i) => el.highlights ? resume_strict[ty][i].highlights = Object.values(el.highlights).map(el => typeof el === "object" ? Object.values(el).flat(): el).flat() : undefined));
+    (resume[ty] || []).map((el, i) => el.highlights ? resume_strict[ty][i].highlights = Object.values(el.highlights).map(el => typeof el === "object" ? Object.values(Object.values(el).map(arr => Array.isArray(arr) ? arr.map(a => a.replace(/^/,'- ')) : arr)).flat(): el).flat(): undefined));
 
 fs.writeFileSync('John_Ailor.json', JSON.stringify(resume_strict, null, 2));
---
+++ shorten.js
@@ -0,0 +1,10 @@
+"use strict";
+const fs = require("fs");
+const args = process.argv.slice(2);
+const resume = JSON.parse(fs.readFileSync(`../${args[0]}`));
+const resume_strict = Object.assign({}, resume);
+
+['work', 'education', 'volunteer', 'project'].map((ty) =>
+    (resume[ty] || []).map((el, i) => el.highlights ? resume_strict[ty][i].highlights = Object.values(el.highlights).map(el => typeof el === "object" ? Object.values(Object.values(el).map(arr => Array.isArray(arr) ? undefined : arr)).flat(): el).flat(): undefined));
+
+fs.writeFileSync('John_Ailor-short.json', JSON.stringify(resume_strict, null, 2));
